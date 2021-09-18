6f379454d8b18f6268b6b75959d7e9cb833c0b7c
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-18 12:25:28 +0200

    First commit

---
+++ Lesson01IntroToOOP/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01IntroToOOP
+{
+    // This class is a blueprint for a datatype
+    class Human
+    {
+        // Member variable
+        // Every single Human will be called Pera unless the name is changed
+        public string firstName = "Pera";
+        public string lastName;
+
+        // Member method
+        public void IntroduceMyself()
+        {
+            if(lastName != null)
+            {
+                Console.WriteLine($"Hi, I'm {firstName} {lastName}.");
+            }
+            else
+            {
+                Console.WriteLine($"Hi, I'm {firstName}.");
+            }
+        }
+    }
+}
---
+++ Lesson01IntroToOOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01IntroToOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An object of Human class/ an instance of Human
+            Human nikola = new Human();
+
+            // Access and edit a variable from outside
+            nikola.firstName = "Nikola";
+            // Call the method from the class
+            nikola.IntroduceMyself();
+
+            Human pera = new Human();
+            // There is no need to set the firstName since the default value is already set to Pera
+            pera.IntroduceMyself();
+
+            Human zika = new Human();
+            zika.firstName = "Zika";
+            zika.lastName = "Jovanovic";
+            zika.IntroduceMyself();
+
+            Human mika = new Human();
+            zika.firstName = "Mika";
+            zika.lastName = "Milovanovic";
+            zika.IntroduceMyself();
+
+            Console.ReadLine();
+        }
+    }
+}
