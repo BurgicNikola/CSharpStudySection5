9e867e5c57da3055a39bc1c47fd8c7f401c67306
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-19 21:43:01 +0200

    Add lesson 3

---
+++ Lesson03GettersAndSetters/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03GettersAndSetters
+{
+    class Box
+    {
+        private int length;
+        public int width;
+        public int height;
+        public int volume;
+
+        public void SetLength(int length)
+        {
+            if(length < 1)
+            {
+                throw new Exception("Length has to be 1 or larger.");
+            }
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public void DisplayVolume()
+        {
+            Console.WriteLine($"The volume of the box is {length * width * height}.");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"The length of the box is {length}, the width is {width}, the height is {height} and the volume is {length * width * height}.");
+        }
+    }
+}
---
+++ Lesson03GettersAndSetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+
+            box.SetLength(9);
+            box.height = 2;
+            box.width = 5;
+
+            box.DisplayInfo();
+            box.DisplayVolume();
+
+            Console.ReadLine();
+        }
+    }
+}
